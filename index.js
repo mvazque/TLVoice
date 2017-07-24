f3bb7f5b798b38c13ae27e754ca3de0eaec8cc11
Miguel Vazquez <mvazque93@gmail.com>
2017-07-24 16:59:53 -0700

    CORS enabled.

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const bodyParser = require('body-parser');
 
 const router = require('./src/router');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+
 // Create Express webapp
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(allowCrossDomain);
 app.use(router);
 
 // Create http server and run it
